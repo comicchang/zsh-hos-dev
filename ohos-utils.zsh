bb9cc05134d282c7d97253106fb3f7a392a840da
Zhang Peng <zhangpeng280@huawei.com>
2022-01-12 17:12:07 +0800

    initial commit
    Signed-off-by: Zhang Peng <zhangpeng280@huawei.com>

---
+++ ohos-utils.zsh
@@ -0,0 +1,102 @@
+# utils for hos development
+
+#==========================================================================
+find-repo-root () {
+    path=$(pwd)
+    while [[ "$path" != "" && ! -e "$path/.repo" ]]; do
+       path=${path%/*}
+    done
+}
+
+function gen-build-path-env()
+{
+    # magic path extracted from build_scripts/build.sh
+    echo "${OHOS_BUILD_TOP}/prebuilts/python/linux-x86/3.8.5/bin:${OHOS_BUILD_TOP}/prebuilts/build-tools/linux-x86/bin"
+}
+
+#==========================================================================
+
+#prepare build environment
+function ohos-conf()
+{
+    local GUESS_OHOS_BUILD_TOP=$(find-repo-root)
+    if [[ -z $GUESS_OHOS_BUILD_TOP ]]; then
+        echo 'error finding repo root'
+        return -1
+    fi
+
+    if [[ $OHOS_BUILD_TOP != $GUESS_OHOS_BUILD_TOP ]] || [ ! -d $OHOS_PRODUCT_OUT ]; then
+        export OHOS_BUILD_TOP=$GUESS_OHOS_BUILD_TOP
+        export OHOS_PRODUCT_OUT=$(ls out -t |grep -P '^(?!preloader|kernel|sdk)'|head -n 1)
+    fi
+
+    if ! ( echo $PATH | grep -q  "prebuilts/python/linux-x86" > /dev/null ); then
+        omz reload
+        export PATH=$(gen-build-path-env):$PATH
+    fi
+    if ! command -v hdc_std &> /dev/null then
+        if [ -f $OHOS_BUILD_TOP/out/sdk/ohos-sdk/linux/toolchains/hdc_std ]; then
+            export PATH=$OHOS_BUILD_TOP/out/sdk/ohos-sdk/linux/toolchains/hdc_std:$PATH
+        else
+            echo "hdc_std not found"
+        fi
+    fi
+
+    return 0
+}
+
+#==========================================================================
+
+function ohos-croot()
+{
+    ohos-conf
+    cd $OHOS_BUILD_TOP
+}
+
+
+# manaully regenerate build.ninja
+function ohos-gn()
+{
+    ohos-conf
+    gn --root=${OHOS_BUILD_TOP} -q gen ${OHOS_PRODUCT_OUT}/
+}
+
+# build
+function ohos-make()
+{
+    ohos-conf
+    if [ ! -f ${OHOS_PRODUCT_OUT}/build.ninja ]; then
+        echo "build.ninja not found, run ohos-gn first"
+    fi
+
+    # disable gn update
+    sed -i \
+        -e 's/generator = 1/generator = 0/' \
+        ${OHOS_PRODUCT_OUT}/build.ninja
+
+    # build with ccache and pycache
+    USE_CCACHE="1" CCACHE_DIR="$HOME/CC_TMP" PYCACHE_DIR="$HOME/.pycache" PYCACHE_ENABLE="true" \
+        ninja -d keepdepfile -d keeprsp -w dupbuild=err -C $OHOS_PRODUCT_OUT $@
+}
+
+function ohos-push()
+{
+    # todo
+    echo "===== done ====="
+}
+
+function ohos-apply()
+{
+    ohos-make "$@" && \
+    ohos-push "$@"
+}
+
+#==========================================================================
+
+function generate-clean-compiledb()
+{
+    ohos-conf
+    ninja -C $OHOS_PRODUCT_OUT -t compdb | \
+        jq --unbuffered 'del(.[] | select(.output|test("(_mingw|_android|_multi|_mac|_windows|Test\/)"))) | del(.[] | select(.output|test("(\\.o)$")|not))' | \
+        > $OHOS_BUILD_TOP/compile_commands.json
+}
